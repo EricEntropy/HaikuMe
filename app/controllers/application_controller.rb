8f991b16cb5e5e8dd7d5fcb960072ed45ef5da55
eric <contreras.eric18@gmail.com>
2021-06-23 01:47:38 -0400

    first actual render

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user
+
+    def home 
+        redirect_to '/login' unless current_user
+    end 
+
+    def current_user
+        session[:username]
+    end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,8 +22,8 @@ Rails.application.routes.draw do
   get 'users/show'
 
   root 'application#home'
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
