5dfde285826ed79a6600f4c9d2038eb7cdd27515
eric <contreras.eric18@gmail.com>
2021-06-24 20:03:03 -0400

    fixed user passwords

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2021_06_21_194715) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "passowrd_digest"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
