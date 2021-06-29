99254721d6c9699ee922a1b63b8f437eefdfb1b4
eric <contreras.eric18@gmail.com>
2021-06-28 22:24:15 -0400

    signup clean up and field_error over written

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module HaikuMe
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+  html_tag
+}
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
